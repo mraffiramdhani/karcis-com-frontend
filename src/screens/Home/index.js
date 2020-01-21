773ac2e854ee1f15dafe64ab342a21739ff25050
Arfifa Rahman <arfifarahman509@gmail.com>
2020-01-22 00:08:15 +0700

    create category menu home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
-import { Text, View, SafeAreaView, StyleSheet } from 'react-native'
+import { Text, View, SafeAreaView, StyleSheet, ScrollView, TouchableOpacity } from 'react-native'
 
 import { Header } from '../../components/Header'
+import BannerHome from '../../components/BannerHome'
+import CategoriesHome from '../../components/CategoriesHome'
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      bannerImage: [require('../../assets/carouselImage/1.png'), require('../../assets/carouselImage/2.png'), require('../../assets/carouselImage/3.png'), require('../../assets/carouselImage/4.png')]
+    }
+  }
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <Header />
-        <View style={styles.body}>
-
-        </View>
+        <ScrollView>
+          <View style={styles.body}>
+            <View>
+              <BannerHome image={this.state.bannerImage} />
+              <TouchableOpacity style={styles.btnSeeAllPromo}>
+                <Text style={styles.textBtnSeeAllPromo}>See All Promo</Text>
+              </TouchableOpacity>
+            </View>
+            <View>
+              <Text style={styles.textSectionTitle}>Hey you, going anywhere?</Text>
+              <CategoriesHome />
+            </View>
+            <View>
+              <Text style={styles.textSectionTitle}>Awali Tahun Baru dengan Staycation!</Text>
+              <Text style={styles.textSectionSubtitle}>Awali Tahun Baru dengan Staycation!</Text>
+              <CategoriesHome />
+            </View>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     )
   }
@@ -18,10 +42,33 @@ class Home extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: '#FFFFFF'
   },
   body: {
     flex: 1
+  },
+  btnSeeAllPromo: {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    position: "relative",
+    width: 90,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+    alignSelf: 'flex-end',
+    borderRadius: 5,
+    right: 16,
+    bottom: 40
+  },
+  textBtnSeeAllPromo: {
+    color: '#fff',
+    fontSize: 12
+  },
+  textSectionTitle: {
+    color: '#35405A',
+    fontSize: 18,
+    fontWeight: 'bold',
+    paddingHorizontal: 16
   }
 })
 
