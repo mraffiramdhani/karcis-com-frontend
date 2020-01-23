304d43a9794bb88bedd5b8d9338c2de65eb91acf
Arfifa Rahman <arfifarahman509@gmail.com>
2020-01-23 10:06:22 +0700

    fix merge

--- src/screens/myorder/index.js
+++ src/screens/myorder/index.js
@@ -1,14 +1,12 @@
-import React, {Component} from 'react';
-import Headers from '../../components/MyOrder/Headers';
-import TabCard from '../../components/MyOrder/TabCard';
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
 
 export default class MyOrder extends Component {
   render() {
     return (
-      <>
-        <Headers />
-        <TabCard />
-      </>
-    );
+      <View>
+        <Text> ini myOrder </Text>
+      </View>
+    )
   }
 }
